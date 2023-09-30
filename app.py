7b4276095f9d5d3fd9d6794467a2111d4e6faee6
mrtomno <adel.ngo@yahoo.com>
2023-09-30 14:53:38 -0500

    Added register form

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 app.app_context().push()
     
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask_feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "BATMANISAWESOME!!!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
+db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
@@ -31,5 +32,16 @@ def register():
     form = UserForm()
     if form.validate_on_submit():
         """take data"""
+        username = form.username.data
+        password = form.username.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
 
-    return render_template("register.html")
+        user = User(username=username, password=password, email=email, first_name=first_name,last_name=last_name)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect("/secret")
+
+    return render_template("register.html", form=form)
